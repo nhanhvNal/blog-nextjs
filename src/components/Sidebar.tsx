37edb9147bc49cdb03c2d96c86a7eef886137bf6
hoang van nhan <nhanhv@nal.vn>
2025-04-08 10:11:26 +0700

    handle paging when create record success

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 const authorAvatar =
-  "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d";
+  "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80";
 
 const categories = [
   { name: "Technology", href: "/category/technology" },
